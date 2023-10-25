a169b2c6088666b6a89583b017ec4a5a12c94e17
Mountain/\Ash <mountainash@rmwpublishing.net>
2023-10-25 12:56:30 +0200

    double quote to single quote

--- src/index.mjs
+++ src/index.mjs
@@ -1,28 +1,28 @@
-import "./styles.css";
+import './styles.css';
 
 let invoice_number = 653;
 
-document.getElementById("invoice_number").innerHTML = invoice_number;
+document.getElementById('invoice_number').innerHTML = invoice_number;
 
 let invoice_total = 60;
 
-document.getElementById("invoice_total").innerHTML = `$${invoice_total}.00`;
+document.getElementById('invoice_total').innerHTML = `$${invoice_total}.00`;
 
 const payment_details_obj = {
-  key: "value",
-  key: "value",
-  key: "value",
+  key: 'value',
+  key: 'value',
+  key: 'value',
 };
 
-let payment_details = "";
+let payment_details = '';
 
 for (let [key, value] of Object.entries(payment_details_obj)) {
   payment_details += `<dt>${key}</dt><dd>${value}</dd>`;
 }
 
-document.getElementById("payment_details").innerHTML = payment_details;
+document.getElementById('payment_details').innerHTML = payment_details;
 
-document.getElementById("invoice_items").innerHTML = `
+document.getElementById('invoice_items').innerHTML = `
   <tr>
   <td>Sample Item</td>
   <td>1</td>
