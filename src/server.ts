bdac5b94fe92f25197682f5ba2a69661992bf7f6
Mountain/\Ash <mountainash@rmwpublishing.net>
2025-02-01 18:07:13 +0100

    👷 Server decision optimisation

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,25 @@
-import indexHtml from './index.html';
-
 const server = Bun.serve({
   async fetch(req) {
     const path = new URL(req.url).pathname;
 
-    if (path === '/') return new Response(Bun.file(indexHtml, { type: 'text/html' }));
-    if (path === '/index.mjs') return new Response(Bun.file('./src/index.mjs', { type: 'text/javascript' }));
-    if (path === '/helpers.mjs') return new Response(Bun.file('./src/helpers.mjs', { type: 'text/javascript' }));
-    if (path === '/styles.css') return new Response(Bun.file('./src/styles.css', { type: 'text/css' }));
+    if (path.startsWith('/images/')) {
+      return new Response(Bun.file(`./public/${path}`, { type: 'image/png' }));
+    }
+
+    switch (path) {
+      case '/':
+        return new Response(Bun.file('./src/index.html', { type: 'text/html' }));
+      case '/index.mjs':
+        return new Response(Bun.file('./src/index.mjs', { type: 'text/javascript' }));
+      case '/helpers.mjs':
+        return new Response(Bun.file('./src/helpers.mjs', { type: 'text/javascript' }));
+      case '/styles.css':
+        return new Response(Bun.file('./src/styles.css', { type: 'text/css' }));
+      default:
+        return new Response('Page not found', { status: 404 });
+    }
 
     // 404s
-    return new Response('Page not found', { status: 404 });
   },
   development: true,
   port: process.env.port || 1234,
